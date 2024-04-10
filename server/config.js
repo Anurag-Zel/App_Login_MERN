5a0688a45580c78294ce7bd7c1bfdd0ebcbb06e2
Anurag Majumdar <lightningphoenix484@gmail.com>
2024-04-10 12:08:55 +0530

    Changed server database

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ export default {
     JWT_SECRET : "z19RWmdWTWgakx/IPtXXI9AJUyfFn81ytX5vH5RpeCY=",
     EMAIL : "etha12@ethereal.email", // texting email
     PASSWORD : "JSvSzMqDmCHMNubUZY",
-    ATLAS_URI : "mongodb+srv://admin:admin123@cluster0.zdyryvu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    ATLAS_URI : "mongodb+srv://lightningphoenix484:YyIP0Rizd27Chb8N@cluster.8wby1y2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster"
 }
